45acfce9e44a8d76e98c15408e83a859a18de450
johannes burström <burstromjohannes@gmail.com>
2019-02-06 14:21:31 +0100

    ScrollingBackground

---
+++ The Great Rescue/Assets/Scripts/RepeatingBG.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepeatingBG : MonoBehaviour
+{
+
+    private BoxCollider2D groundCollider;       //This stores a reference to the collider attached to the Ground.
+    private float groundHorizontalLength;       //A float to store the x-axis length of the collider2D attached to the Ground GameObject.
+
+    //Awake is called before Start.
+    private void Awake()
+    {
+        //Get and store a reference to the collider2D attached to Ground.
+        groundCollider = GetComponent<BoxCollider2D>();
+        //Store the size of the collider along the x axis (its length in units).
+        groundHorizontalLength = groundCollider.size.x;
+    }
+
+    //Update runs once per frame
+    private void Update()
+    {
+        if (transform.position.x < -groundHorizontalLength)
+        {
+            //if true then the background will run the repeat method.
+            RepositionBackground();
+        }
+    }
+
+    //Moves the object this script is attached to right in order to create our looping background effect.
+    private void RepositionBackground()
+    {
+        //change the *2f value if we want more or less than twice before repeat
+        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffSet;
+    }
+}
---
+++ The Great Rescue/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //make the object move in set speed
+        rb2d.velocity = new Vector2(speed, 0);
+    }
+
+    void Update()
+    {
+        // If the game is over, stop scrolling.
+        //if (-GAMEOVER-)
+        //{
+        //    rb2d.velocity = Vector2.zero;
+        //}
+    }
+}
