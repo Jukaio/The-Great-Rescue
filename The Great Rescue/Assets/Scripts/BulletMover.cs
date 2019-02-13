81182cd43317418c2dbe1259d1c9156abc8376ff
David Naußed <David.Naussed.5395@student.uu.se>
2019-02-13 13:15:34 +0100

    some changes

--- The Great Rescue/Assets/Scripts/BulletMover.cs
+++ The Great Rescue/Assets/Scripts/BulletMover.cs
@@ -13,7 +13,7 @@ public class BulletMover : MonoBehaviour
     void Start()
     {
         bulletMovement = gameObject.AddComponent<Movement>();
-        if(GameObject.Find("Player").GetComponent<PlayerController>().damageBuff== true)
+        if (GameObject.Find("Player").GetComponent<PlayerController>().damageBuff == true)
         {
             damage *= GameObject.Find("Player").GetComponent<PlayerController>().damageMultiplier;
         }
@@ -23,21 +23,33 @@ public class BulletMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(tag == "PlayerBullet")
+        if (tag == "PlayerBullet")
         {
             bulletMovement.moveRight(speed);
 
             if (gameObject.transform.position.x >= 11)
                 Destroy(gameObject);
         }
-        else if(tag == "Enemy")
+        else if (tag == "Enemy")
         {
             bulletMovement.moveLeft(speed);
 
             if (gameObject.transform.position.x <= -11)
                 Destroy(gameObject);
         }
-        
-    }
 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (tag == "PlayerBullet")
+        {
+            if (collision.gameObject.tag == "Enemy" && collision.gameObject.layer != 8)
+                Destroy(gameObject);
+        }
+        else if (tag == "Enemy")
+        {
+            if (collision.gameObject.tag == "Player" && collision.gameObject.layer != 8)
+                Destroy(gameObject);
+        }
+    }
 }
