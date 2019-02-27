80d2022e3fe333df89aa4e51805d9d4395ea2948
johannes burström <burstromjohannes@gmail.com>
2019-02-27 12:01:54 +0100

    Main Menu, Powerup CD, Restart Game, Power up Icons
    Press R to restart and Esc to go to main menu

---
+++ The Great Rescue/Assets/Scripts/PowerUps/Cooldown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cooldown : MonoBehaviour
+{
+    public Image cooldown;
+    public float waitTime = 10;
+    public bool coolingDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(coolingDown == true)
+        {
+            cooldown.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+        }
+    }
+}
---
+++ The Great Rescue/Assets/Scripts/PowerUps/RotatingPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPowerUp : MonoBehaviour
+{
+    float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0.5f;  //velocity
+    }
+
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(0, x, 0)); //applying rotation
+    }
+}
