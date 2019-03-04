768e2ac4ca0a93264c5a73f2f99add98c73c56a2
Johannes <superollie1337@gmail.com>
2019-03-04 11:31:08 +0100

    BloodFXFix

---
+++ The Great Rescue/Assets/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(GetComponent<ParticleSystem>().duration);
+        Destroy(gameObject);
+    }
+}
