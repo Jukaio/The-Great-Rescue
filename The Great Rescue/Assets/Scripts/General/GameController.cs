a5e41b882d604b2ed70a74d150263f568fa57616
johannes burström <burstromjohannes@gmail.com>
2019-03-07 12:00:54 +0100

    Minor fixes

---
+++ The Great Rescue/Assets/Scripts/General/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+        public GameObject[] toSpawn;
+        public Vector3 spawnValues;
+        public int hazardCount;
+        public float spawnWait;
+        public float startWait;
+        public float waveWait;
+
+        public int arrayLength;
+
+        void Start()
+        {
+            StartCoroutine(SpawnWaves());
+            arrayLength = toSpawn.Length;
+        }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = gameObject.transform.position;
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(toSpawn[Random.Range(0, arrayLength)], spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
---
+++ The Great Rescue/Assets/Scripts/General/HigScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HigScore : MonoBehaviour
+{
+    public float highscore;
+    public GameObject[] enemies;
+    public Text highscoreText;
+
+
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+            if (enemy.GetComponent<enemyStatus>().healthPoints <= 0)
+                highscore += 10;
+        highscoreText.text = "Score: " + highscore;
+    }
+}
