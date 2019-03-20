dad59f1dcc2fd897b6b3a60f4123eca9940d5be2
johannes burström <burstromjohannes@gmail.com>
2019-03-20 17:11:56 +0100

    Added 2nd obstacle

---
+++ The Great Rescue/Assets/Scripts/Enemy/Obstacles/TrapAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class TrapAttack : MonoBehaviour
+{
+    public GameObject blood;
+    Animator animator = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<Animator>().SetBool("trapActivated", true);
+            Instantiate(blood, transform.position, Quaternion.identity);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
