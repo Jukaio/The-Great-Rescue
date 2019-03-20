530bdf19ca0840a9a61baa6b6681398390bdb8f7
johannes burström <burstromjohannes@gmail.com>
2019-03-20 15:37:50 +0100

    Refined order layers and bug squashing

---
+++ The Great Rescue/Assets/Scripts/General/DestroyOnX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnX : MonoBehaviour
+{
+    public GameObject gO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gO.transform.position.x <= -11)
+            Destroy(gO);
+    }
+}
