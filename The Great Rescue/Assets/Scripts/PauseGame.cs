99845c5b6ee37c2b0bb3cb842fc3a9572043b45d
David Naußed <David.Naussed.5395@student.uu.se>
2019-03-21 14:15:42 +0100

    Start Menu, Credits, Button Presses

---
+++ The Great Rescue/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //uses the p button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+            else if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+
+    //Reloads the Level
+
+    //controls the pausing of the scene
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
--- The Great Rescue/Assets/Scripts/UI/MainMenu.cs
+++ The Great Rescue/Assets/Scripts/UI/MainMenu.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Credits;
+
     // Start is called before the first frame update
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenCredit()
+    {
+        Credits.SetActive(true);
+    }
+
+    public void CloseCredit()
+    {
+        Credits.SetActive(false);
     }
 }
